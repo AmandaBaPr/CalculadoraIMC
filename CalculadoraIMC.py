1d5835fee491a457b7600f4eec90ea3336d42da2
AmandaBaPr <133283935+AmandaBaPr@users.noreply.github.com>
2023-05-11 19:49:29 -0300

    Arquivo adicionado ao repositório

---
+++ CalculadoraIMC.py
@@ -0,0 +1,31 @@
+#Desenvolva uma calculadora de IMC, o programa deve pedir o peso e a altura ao usuario.
+#Calcular o IMC e retornar para o usuario o IMC e a categoria em que se encontra
+
+#Menor que 18,5 - Abaixo do peso
+#Entre 18,5 e  24,9 - Peso normal
+#Entre 25 e 29,9 - Sobrepeso
+#Igual ou acima de 30 - Obesidade
+
+def calcular (peso, altura):
+    imc = peso / (altura ** 2)
+    return imc
+
+def determinar (imc):
+    if imc < 18.5:
+        categoria = "Abaixo do Peso"
+    elif imc < 25:
+        categoria = "Normal"
+    elif imc < 30:
+        categoria = "Sobrepeso"
+    else:
+        categoria = "Obesidade"
+    return categoria
+
+peso = input("Digite seu peso: ")
+altura = input("Digite sua altura: ")
+
+imc= calcular (peso, altura)
+
+categoria = determinar (imc)
+
+print("Seu imc é",imc)
